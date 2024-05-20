b1bd4a5ac6c5c7dcfb2c756f6397d04959319167
Simrath Randhawa <srandhawa1250@gmail.com>
2024-05-20 11:07:57 -0500

    1107 5-20 first commit adding basic files

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    UserProfileComponent,
+    UserRegistrationComponent
   ],
   imports: [
     BrowserModule,
