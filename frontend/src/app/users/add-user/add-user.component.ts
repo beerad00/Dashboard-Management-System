44cd6686b17bad978a31b9ab90b58cd8ff453cd8
Jakereed292 <79810317+Jakereed292@users.noreply.github.com>
2024-05-24 02:57:02 -0400

    added user email validation

--- frontend/src/app/users/add-user/add-user.component.ts
+++ frontend/src/app/users/add-user/add-user.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { UserRequestDto } from '../../models/user-request.dto';
 import { createUserDto } from '../../models/createUserDto';
+import { CompanyService } from 'src/app/services/company.service';
 
 @Component({
   selector: 'app-add-user',
@@ -23,18 +24,27 @@ export class AddUserComponent implements OnInit {
   confirmPassword: string = '';
   companyId: number | null = null;
   passwordError: boolean = false;
+  emailAvailability: boolean = true;
+  emailError: boolean = true;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router, private companyService: CompanyService) {}
 
   ngOnInit(): void {
     this.companyId = this.authService.getCurrentCompanyId();
-   
   }
 
   async onSubmit() {
     if (this.userRequest.password !== this.confirmPassword) {
       this.passwordError = true;
       console.error('Passwords do not match');
+      return;
+    }
+  
+    const emailTaken = await this.checkEmailAvailability(this.userRequest.email);
+    this.emailAvailability = !emailTaken;
+  
+    if (!this.emailAvailability) {
+      console.error('Email Taken');
       return;
     }
 
@@ -54,5 +64,25 @@ export class AddUserComponent implements OnInit {
 
   navigateToUsers() {
     this.router.navigate(['/users']);
+  }
+
+  async checkEmailAvailability(email: string): Promise<boolean> {
+    if (!this.companyId) {
+      console.error('No company ID selected');
+      return false;
+    }
+  
+    try {
+      const users = await this.companyService.getUsersByCompanyId(this.companyId);
+      const userEmails = users.map(user => user.profile.email);
+  
+      console.log('User Emails:', userEmails);
+      console.log('Checking Email:', email);
+  
+      return userEmails.includes(email);
+    } catch (error) {
+      console.error('Error fetching users:', error);
+      return false;
+    }
   }
 }
